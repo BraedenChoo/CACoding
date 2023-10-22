2458f8cb767d0b090ff62d92fc877b1f5f985265
BraedenChoo <braeden.choo@mail.utoronto.ca>
2023-10-22 19:17:09 -0400

    output boundary named changed

--- src/use_case/clear_users/ClearOutputBoundary.java
+++ src/use_case/clear_users/ClearOutputBoundary.java
@@ -5,5 +5,5 @@ package use_case.clear_users;
 import use_case.login.LoginOutputData;
 
 public interface ClearOutputBoundary {
-    void prepareView(ClearOutputData deletedUsers);
+    void prepareView(ClearOutputData clearOutputData);
 }
